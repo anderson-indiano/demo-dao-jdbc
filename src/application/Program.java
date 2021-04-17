c17fd73431c0dab5df006f537d15d9eaee29a3a4
anderson-indiano <indianobox2@gmail.com>
2021-04-16 22:30:52 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Program.java
+++ src/application/Program.java
@@ -2,6 +2,8 @@ package application;
 
 import java.util.Date;
 
+import model.dao.SellerDao;
+import model.dao.impl.SellerDaoJDBC;
 import model.entities.Department;
 import model.entities.Seller;
 
@@ -13,6 +15,8 @@ public class Program {
 		
 		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, department);
 		System.out.println(seller);
+		
+		SellerDao sellerDao = new SellerDaoJDBC();
 	}
 
 }
